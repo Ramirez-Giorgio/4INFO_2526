d884aef216da86b726aba55796a54560e4da17e3
Ramirez-Giorgio <giorgio.ramirez@outlook.it>
2025-11-06 15:23:22 +0100

    es09_ramirez

--- TPSI/ramirez_giorgio/javascript/ramirez_esercizio_js_09.js
+++ TPSI/ramirez_giorgio/javascript/ramirez_esercizio_js_09.js
@@ -1,7 +1,7 @@
 let username = prompt("Inserisci username:");
 
 (username == null) && alert("Canceled") ||
-(username != "Admin") && alert("I don’t know you") ||
+(username != "Admin") && alert("I don't know you") ||
 (username == "Admin") && (
   (password = prompt("Password:")),
   (password == null && alert("Canceled")) ||
