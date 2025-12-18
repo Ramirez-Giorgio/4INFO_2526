f2f4a9c09305a74a0e9338f39aff11e6d7761af1
Ramirez-Giorgio <giorgio.ramirez@outlook.it>
2025-12-18 13:09:06 +0100

    es_11-14(incompleti)_ramirez

---
+++ TPSI/ramirez_giorgio/javascript/ramirez_esercizio_js_11.js
@@ -0,0 +1,8 @@
+let Automobile = function (nome,modello,marca,anno,colore){
+    ((this.nome) = nome),
+    ((this.cognome) = cognome),
+    ((this.modello) = modello),
+    ((this.marca) = marca),
+    ((this.anno) = anno),
+    ((this.colore) = colore)   
+}
