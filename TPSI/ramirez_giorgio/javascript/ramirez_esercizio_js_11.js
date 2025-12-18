9072bd30ea7aae4b1b2c32ab49779e71f0409a6c
Ramirez-Giorgio <giorgio.ramirez@outlook.it>
2025-12-18 13:29:58 +0100

    es11_ramirez

--- TPSI/ramirez_giorgio/javascript/ramirez_esercizio_js_11.js
+++ TPSI/ramirez_giorgio/javascript/ramirez_esercizio_js_11.js
@@ -1,8 +1,12 @@
 let Automobile = function (nome,modello,marca,anno,colore){
-    ((this.nome) = nome),
-    ((this.cognome) = cognome),
-    ((this.modello) = modello),
-    ((this.marca) = marca),
-    ((this.anno) = anno),
-    ((this.colore) = colore)   
+    (this.nome = nome),
+    (this.cognome = cognome),
+    (this.modello = modello),
+    (this.marca = marca),
+    (this.anno= anno),
+    (this.colore = colore),
+    (this.speak=function(){
+        console.log("la macchina ${nome}, di marca ${marca}, modello ${anno} ed anno , ha il colore ${colore}")
+    })
 }
+
