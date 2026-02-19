eeb4cc4b87b4e8571f2044e896e67dbb1f0f8125
Ramirez-Giorgio <giorgio.ramirez@outlook.it>
2026-02-19 16:18:44 +0100

    es 39 tdd

--- TPSI/ramirez_giorgio/esercizi_tdd_STUDENTI/esercizio_js_39/soluzione.js
+++ TPSI/ramirez_giorgio/esercizi_tdd_STUDENTI/esercizio_js_39/soluzione.js
@@ -3,7 +3,7 @@ export const reverse = (l) => {
     for(let el of l){
         lnuovo.unshift(el)
     }
-    let somma = l.reduce((acc, current) => acc + current, 0); // acc = acc + current^2
+    let somma = l.reduce((acc, current) => acc + current, 0); // acc = acc + current
     lnuovo.unshift(somma)
     return lnuovo
 }
