abc5db82927ede13b3fd54c87c4a0dea547bfc29
Ramirez-Giorgio <giorgio.ramirez@outlook.it>
2026-03-19 16:29:58 +0100

    es66

---
+++ TPSI/ramirez_giorgio/javascript/ramirez_esercizio_js_66.js
@@ -0,0 +1,5 @@
+function visualizza_studente({name, price, quantity}) {
+    console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
+}
+let item = ["Egg", 0.25, 12];
+visualizza_studente(item)
