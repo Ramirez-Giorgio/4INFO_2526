7610d1237941a7bc2e212a74099789f4ca289738
Ramirez-Giorgio <giorgio.ramirez@outlook.it>
2026-04-22 23:29:40 +0200

    es 70-71

--- TPSI/ramirez_giorgio/esercizi_tdd_STUDENTI/esercizio_js_70/soluzione.js
+++ TPSI/ramirez_giorgio/esercizi_tdd_STUDENTI/esercizio_js_70/soluzione.js
@@ -1,3 +1,23 @@
 export class Calcolatrice {
- //INSERISCI QUI IL TUO CODICE
+    somma(a, b) { return a + b; }
+    sottrazione(a, b) { return a - b; }
+    moltiplicazione(a, b) { return a * b; }
+    divisione(a, b) { 
+        if (b === 0) throw new Error("Divisione per zero");
+        return a / b; 
+    }
 }
+
+const calc = new Calcolatrice();
+
+const espressioni = [
+    "6 - 2^* 5 + 8 / 4",
+    "11^* 3 + 5 - 5 / 11",
+    "29 + 2 + (-7) / 2 + 1^* 2" 
+];
+
+console.log("RISULTATI CALCOLATRICE (Senza precedenze):");
+espressioni.forEach(espr => {
+        const res = calc.eseguiEspressione(espr);
+        console.log(`${espr} = ${res}`);
+});
--- TPSI/ramirez_giorgio/esercizi_tdd_STUDENTI/esercizio_js_71/soluzione.js
+++ TPSI/ramirez_giorgio/esercizi_tdd_STUDENTI/esercizio_js_71/soluzione.js
@@ -1,27 +1,75 @@
 export class Poligono {
-   //INSERISCI QUI IL TUO CODICE
-}
+   constructor(lato, nlati, nfisso) {
+      this.lato = lato;
+      this.nlati = nlati;
+      this.nfisso = nfisso;   
+   }
 
-export class Quadrato extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+   perimetro() {
+      let p = this.lato * this.nlati;
+      console.log(`Perimetro: ${p} cm`);  
+      return p;
+   }
+
+   area() {
+      let p = this.lato * this.nlati;
+      let apotema = this.lato * this.nfisso;
+      let a = (p * apotema) / 2;
+      console.log(`Area: ${Math.round(a)} cm^2`);
+      return a;
+   }
 }
 
 export class TriangoloEquilatero extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+    constructor(lato) { super(lato, 3, 0.289); }
+}
+
+export class Quadrato extends Poligono {
+    constructor(lato) { super(lato, 4, 0.5); }
 }
 
 export class Pentagono extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+    constructor(lato) { super(lato, 5, 0.688); }
 }
 
 export class Esagono extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+    constructor(lato) { super(lato, 6, 0.866); }
 }
 
 export class Ettagono extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+    constructor(lato) { super(lato, 7, 1.038); }
 }
 
 export class Ottagono extends Poligono {
-   //INSERISCI QUI IL TUO CODICE
+    constructor(lato) { super(lato, 8, 1.207); }
 }
+
+console.log("Triangolo Equilatero lato 15 cm ");
+let tri = new TriangoloEquilatero(15);
+tri.perimetro();
+tri.area();
+
+console.log("\nQuadratlato lato 10 cm");
+let qua = new Quadrato(10);
+qua.perimetro();
+qua.area();
+
+console.log("\nPentagono lato 21 cm");
+let pen = new Pentagono(21);
+pen.perimetro();
+pen.area();
+
+console.log("\nEsagono lato 18 cm");
+let esa = new Esagono(18);
+esa.perimetro();
+esa.area();
+
+console.log("\nEttagono lato 9 cm");
+let ett = new Ettagono(9);
+ett.perimetro();
+ett.area();
+
+console.log("\nOttagono lato 12 cm");
+let ott = new Ottagono(12);
+ott.perimetro();
+ott.area();
