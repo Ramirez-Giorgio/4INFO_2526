c183ef776503f0217757b1caaeba0612c9bad534
Ramirez-Giorgio <giorgio.ramirez@outlook.it>
2026-04-23 15:42:51 +0200

    es72

--- TPSI/ramirez_giorgio/esercizi_tdd_STUDENTI/esercizio_js_72/soluzione.js
+++ TPSI/ramirez_giorgio/esercizi_tdd_STUDENTI/esercizio_js_72/soluzione.js
@@ -1,11 +1,22 @@
 export class Studente {
-  //INSERISCI QUI IL TUO CODICE
+  constructor(nome, classe) {
+    this.nome = nome;
+    this.classe = classe;}
 }
 
 export class Docente {
-  //INSERISCI QUI IL TUO CODICE
+  constructor(nome,classe){
+    this.nome = nome
+    this.classe = classe
+  }
 }
 
 export const confrontaOBJ = (obj1, obj2) => {
-  //INSERISCI QUI IL TUO CODICE
-};
+  if ((obj1 instanceof Studente && obj2 instanceof Studente) || (obj1 instanceof Docente && obj2 instanceof Docente)){
+    if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+      return true}
+   else{return false} 
+  }
+  else
+  {return false} 
+}
